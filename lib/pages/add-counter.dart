9b9ab5ebdf9ad91c75c2c7f4364a424bf66d18ca
igieske <igoza99@gmail.com>
2024-04-02 23:43:39 +0300

    Initial add AddCounterPage widget.

---
+++ lib/pages/add-counter.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class AddCounterPage extends StatelessWidget {
+  const AddCounterPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Center(child: Text('add counter page'));
+  }
+}
